c78b77a71162acc5193fe7d075984dbc732403a0
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-03-12 10:44:12 +1100

    Initial commit

---
+++ src/client/java/mct32/mct32sarmorhud/MCT32sArmorHUDClient.java
@@ -0,0 +1,10 @@
+package mct32.mct32sarmorhud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCT32sArmorHUDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
